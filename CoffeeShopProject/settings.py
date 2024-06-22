9169bc868c44bf4ef45b984a9eb45bff9b9402bb
mahdi shafaat <mahdishafaat1384@gmail.com>
2024-06-22 15:30:54 +0330

    create product form

--- CoffeeShopProject/settings.py
+++ CoffeeShopProject/settings.py
@@ -109,7 +109,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'mainapp/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
