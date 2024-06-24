3c18ae38e206839f496a475409138e73153106c2
Tyrmo <itsTyrmo@hotmail.com>
2024-06-25 00:52:48 +0330

    changes in database

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,11 +1,20 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, HttpResponse
 from django.views.generic import CreateView
 from mainapp.models import Product
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 def homepage(request):
     return render(request, 'index/index.html', {})
 
-class ProductCreateView(CreateView):
+
+class StaffRequiredMixin(UserPassesTestMixin):
+    def test_func(self):
+        return self.request.user.groups.filter(name='Staff').exists()
+
+    def handle_no_permission(self):
+        return HttpResponse("You do not have permission to view this page")
+
+class ProductCreateView(LoginRequiredMixin, StaffRequiredMixin, CreateView):
     model = Product
     success_url = reverse_lazy('index')
     fields="__all__"
