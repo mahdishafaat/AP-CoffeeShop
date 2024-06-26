955a44ee5773a754c833f1a2e6a950b3b5e63834
Tyrmo <itsTyrmo@hotmail.com>
2024-06-26 13:50:15 +0330

    removed unnecessary file logedout.html and made mainapp/urls.py more professional

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 from . import views
-from mainapp.views import ProductCreateView,store_view
 
 urlpatterns = [
     path('', views.homepage, name='index'),
-    path('create_product/', ProductCreateView.as_view(), name='create product'),
-    path('store/',store_view, name='store')
+    path('create_product/', views.ProductCreateView.as_view(), name='create product'),
+    path('store/', views.store_view, name='store')
 ]
