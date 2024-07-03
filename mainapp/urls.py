004e2eebe9d4280888a3492a8b2dd02c689edb44
Tyrmo <itsTyrmo@hotmail.com>
2024-07-03 12:33:05 +0330

    added navigation menu to management panel and linked select_storage and create_product views to it

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.homepage, name='index'),
-    path('create_product/', views.ProductCreateView.as_view(), name='create product'),
+    path('create_product/', views.ProductCreateView.as_view(), name='create_product'),
     path('store/', views.store_view, name='store'),
     path('management/', views.management_panel, name='management_panel'),
     path('select_storage/', views.select_storage_view, name='select_storage'),
